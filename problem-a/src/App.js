c25afabd7b6ca621e3f1ba3633e5b8a29e9077b9
wendelli-1839314 <wendelli@uw.edu>
2020-08-17 21:32:22 -0700

    finished

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -5,4 +5,58 @@ const EXAMPLE_SENATORS = [
   { id: 'M001111', name: 'Patty Murray', state: 'WA', party: 'Democrat', phone: '202-224-2621', twitter: 'PattyMurray' }
 ];
 
-/* Your code goes here */
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>US Senators 2019</h1>
+        <SenatorTable senators={this.props.senators} />
+      </div>
+    );
+  }
+}
+
+export class SenatorTable extends Component {
+  render() {
+    let senatorRows = this.props.senators.map((senator) => <SenatorRow key={senator.name} senator={senator} />)
+    return (
+      <table className="table table-bordered">
+        <TableHeader cols={['Name', 'State', 'Phone', 'Twitter']} />
+        <tbody>
+          {senatorRows} 
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export class TableHeader extends Component {
+  render() {
+    let colNames = this.props.cols.map((colName) => <th key={colName}>{colName}</th>)
+    return (
+      <thead>
+        <tr>
+          {colNames}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export class SenatorRow extends Component {
+  render() {
+    let name = this.props.senator.name;
+    let state = this.props.senator.state;
+    let party = this.props.senator.party;
+    let phone = this.props.senator.phone;
+    let twitter = this.props.senator.twitter;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{party.substring(0, 1) + " - " + state}</td>
+        <td><a href={"tel:" + phone}>{phone}</a></td>
+        <td><a href={"https://twitter.com/" + twitter}>{"@" + twitter}</a></td>
+      </tr>
+    );
+  }
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -2,3 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //render the App component here!
+import { App } from "./App";
+import senators from "./senators";
+
+ReactDOM.render(<App senators = { senators }/>, document.getElementById("root"));
